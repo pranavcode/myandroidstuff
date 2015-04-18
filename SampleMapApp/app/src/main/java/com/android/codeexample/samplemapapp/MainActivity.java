1838b752236579a8a4619688ed931c50cdde4764
Pranav Kulkarni <pranavcode@gmail.com>
2015-04-18 19:00:48 +0530

    SampleMapApp now accepts LatLng. Runs on emulator but not on device.

--- SampleMapApp/app/src/main/java/com/android/codeexample/samplemapapp/MainActivity.java
+++ SampleMapApp/app/src/main/java/com/android/codeexample/samplemapapp/MainActivity.java
@@ -21,11 +21,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends ActionBarActivity {
 
-//    String textLat;
-//    String textLon;
-    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-    static final LatLng KIEL = new LatLng(53.551, 9.993);
-
+    double lat;
+    double lng;
     private GoogleMap map;
 
     @Override
@@ -33,51 +30,39 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-//        LocationListener ll = new myLocationListener();
-//
-//        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationListener ll = new myLocationListener();
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
 
         map = ((MapFragment) getFragmentManager().findFragmentById((R.id.map))).getMap();
-
-        if(map!=null) {
-            Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG).title("Hamburg"));
-            Marker kiel = map.addMarker(new MarkerOptions()
-                    .position(KIEL)
-                    .title("Kiel")
-                    .snippet("Kiel is cool")
-                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_plusone_medium_off_client)));
-        }
     }
 
-//    private class myLocationListener implements LocationListener {
-//        @Override
-//        public void onLocationChanged(Location location) {
-//            if(location != null) {
-//                double lat = location.getLatitude();
-//                double lon = location.getLongitude();
-//
-//                textLat = Double.toString(lat);
-//                textLon = Double.toString(lon);
-//
-//
-//
-//            }
-//        }
-//
-//        @Override
-//        public void onStatusChanged(String s, int i, Bundle bundle) {
-//
-//        }
-//
-//        @Override
-//        public void onProviderEnabled(String s) {
-//
-//        }
-//
-//        @Override
-//        public void onProviderDisabled(String s) {
-//
-//        }
-//    }
+    private class myLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location location) {
+            if(location != null) {
+                lat = location.getLatitude();
+                lng = location.getLongitude();
+
+                if(map!=null) {
+                    Marker somemarker = map.addMarker(new MarkerOptions().position(new LatLng(lat, lng)));
+                }
+            }
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    }
 }
